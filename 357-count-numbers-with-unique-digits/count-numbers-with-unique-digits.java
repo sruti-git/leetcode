603810489dd044a5a21140ae09de4fc52425194e
sruti-git <srutiselvi0411@gmail.com>
2026-01-13 19:08:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (60.78%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;
+        int result = 10;
+        int uniqueDigits = 9;
+        int availableDigits = 9;
+        while (n > 1 && availableDigits > 0) {
+            uniqueDigits *= availableDigits;
+            result += uniqueDigits;
+            availableDigits--;
+            n--;
+        }
+        return result;
+    }
+}
